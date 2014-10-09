c998bcc2c83d1ecf4ab1d89ecd48fee0389415ef
mugabe <sadsanta@gmail.com>
2014-10-09 18:39:56 +0700

    s3 storage

--- classes/Kohana/Storage/Connection/S3.php
+++ classes/Kohana/Storage/Connection/S3.php
@@ -1,229 +1,216 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+
+use Aws\S3\S3Client;
+
 /**
  * AWS S3 driver for Storage Module
- * 
+ *
  * @package		Storage
  * @category	Base
- * @author		Micheal Morgan <micheal@morgan.ly>
- * @copyright	(c) 2011-2014 Micheal Morgan
  * @license		MIT
  */
 class Kohana_Storage_Connection_S3 extends Storage_Connection
 {
-	/**
-	 * Default config
-	 * 
-	 * AWS access credentials are located in the AWS Portal. Reference "config/storage.php" for 
-	 * information on config.
-	 * 
-	 * @access	protected
-	 * @var		array
-	 */
-	protected $_config = array
-	(
-		'key'					=> NULL,
-		'secret'				=> NULL,
-		'bucket'				=> NULL,
-		'cname'					=> NULL,
-		'public'				=> FALSE, 
-		'preauth'				=> 30,
-		'path_style'			=> FALSE,
-		'certificate_authority'	=> FALSE
-	);
-	
-	/**
-	 * AmazonS3 from AWS SDK
-	 * 
-	 * @access	protected
-	 * @var		AmazonS3
-	 */
-	protected $_driver;
+    /**
+     * Default config
+     *
+     * AWS access credentials are located in the AWS Portal. Reference "config/storage.php" for
+     * information on config.
+     *
+     * @access	protected
+     * @var		array
+     */
+    protected $_config = array
+    (
+        'key'					=> NULL,
+        'secret'				=> NULL,
+        'bucket'				=> NULL,
+        'public'				=> FALSE,
+    );
 
-	/**
-	 * Default S3 URL
-	 * 
-	 * @access	protected
-	 * @var		string
-	 */
-	protected $_url = '.s3.amazonaws.com/';
-	
-	/**
-	 * Load connection
-	 * 
-	 * @access	protected
-	 * @return	AmazonS3
-	 */
-	protected function _load()
-	{
-		if ($this->_driver === NULL)
-		{
-			require_once Kohana::find_file('vendor', 'aws-sdk/sdk.class');
+    /**
+     * AmazonS3 from AWS SDK
+     *
+     * @access	protected
+     * @var		AmazonS3
+     */
 
-			$this->_driver = new AmazonS3(Arr::extract($this->_config, 
-				array('key', 'secret', 'certificate_authority')));
+    /**
+     * @var S3Client
+     */
+    protected $_driver;
 
-			$this->_driver->enable_path_style($this->_config['path_style']);
-		}
-		
-		return $this->_driver;
-	}
-	
-	/**
-	 * Set
-	 * 
-	 * @access	protected
-	 * @param	string
-	 * @param	resource
-	 * @param	string
-	 * @return	void
-	 */
-	protected function _set($path, $handle, $mime)
-	{
-		$this->_load();
-		
-		$acl = $this->_config['public'] ? AmazonS3::ACL_PUBLIC : AmazonS3::ACL_PRIVATE;
-		
-		$this->_driver->create_object($this->_config['bucket'], $path, 
-			array('fileUpload' => $handle, 'acl' => $acl, 'contentType' => $mime));
-	}
+    /**
+     * Default S3 URL
+     *
+     * @access	protected
+     * @var		string
+     */
+    protected $_url = '.s3.amazonaws.com/';
 
-	/**
-	 * Get
-	 * 
-	 * @access	protected
-	 * @param	string
-	 * @param	resource
-	 * @return	bool
-	 */
-	protected function _get($path, $handle)
-	{
-		$this->_load();
-		
-		$this->_driver->get_object($this->_config['bucket'], $path, 
-			array('fileDownload' => $handle));
-		
-		return TRUE;
-	}	
-	
-	/**
-	 * Delete
-	 * 
-	 * @access	protected
-	 * @param	string
-	 * @return	bool
-	 */
-	protected function _delete($path)
-	{
-		$this->_load();
-		
-		$this->_driver->delete_object($this->_config['bucket'], $path);
-		
-		return TRUE;
-	}
-	
-	/**
-	 * Size
-	 * 
-	 * @access	protected
-	 * @param	string
-	 * @return	int
-	 */
-	protected function _size($path)
-	{
-		$this->_load();
-		
-		return $this->_driver->get_object_filesize($this->_config['bucket'], $path, FALSE);
-	}
-	
-	/**
-	 * Whether or not file exists
-	 * 
-	 * @access	protected
-	 * @param	string
-	 * @return	bool
-	 */
-	protected function _exists($path)
-	{
-		$this->_load();
-		
-		return $this->_driver->if_object_exists($this->_config['bucket'], $path);
-	}
-	
-	/**
-	 * Get URL
-	 * 
-	 * @access	protected
-	 * @param	string	Path of file 
-	 * @param	string	Protocol to prefix to public URL
-	 * @return	string
-	 */
-	protected function _url($path, $protocol)
-	{
-		if ( ! $this->_config['public'])
-		{
-			$this->_load();
-			
-			return $this->_driver->get_object($this->_config['bucket'], $path, 
-				array('preauth' => time() + $this->_config['preauth']));
-		}
-		else
-			return $protocol . '://' . $this->_get_url() . $path;
-	}
-	
-	/**
-	 * Get listing
-	 * 
-	 * @access	protected
-	 * @param	string	Path of file 
-	 * @return	mixed
-	 */
-	protected function _listing($path, $listing)
-	{
-		$this->_load();
-		
-		$config = array('delimiter' => Storage::DELIMITER);
+    /**
+     * Load connection
+     *
+     * @access	protected
+     * @return	AmazonS3
+     */
+    protected function _load()
+    {
+        if ($this->_driver === NULL)
+        {
+            $this->_driver = S3Client::factory(Arr::extract($this->_config,
+                array('key', 'secret')));
+        }
 
-		if ($path !== NULL)
-		{
-			// Prefix requires trailing delimiter "path/"
-			$config['prefix'] = $path . Storage::DELIMITER;
-		}
-		
-		$response = $this->_driver->list_objects($this->_config['bucket'], $config);
+        return $this->_driver;
+    }
 
-		foreach ($response->body->CommonPrefixes as $directory)
-		{
-			$name = (string) $directory->Prefix;
+    /**
+     * Write content to file. If file already exists, it will be overwritten.
+     *
+     * @access    protected
+     * @param    string
+     * @param    resource
+     * @param    string
+     * @return    void
+     */
+    protected function _set($path, $handle, $mime)
+    {
+        $this->_load();
 
-			$object = Storage_Directory::factory($name, $this);
-			
-			$listing->set($object);
-		}
-		
-		foreach ($response->body->Contents as $file)
-		{
-			$name = (string) $file->Key;
-			
-			$object = Storage_File::factory($name, $this)
-				->size((int) $file->Size)
-				->modified(strtotime($file->LastModified));
-		
-			$listing->set($object);
-		}
-		
-		return $listing;
-	}
-	
-	/**
-	 * Get URL based on whether CNAME is defined or default to S3 bucket URL
-	 * 
-	 * @access	protected
-	 * @return	string
-	 */
-	protected function _get_url()
-	{
-		return ($this->_config['cname']) 
-			? $this->_config['cname'] 
-			: $this->_config['bucket'] . $this->_url;
-	}
+        $this->_driver->putObject(array(
+            'ACL' => $this->_config['public'] ? 'public-read' : 'private',
+            'Bucket' => $this->_config['bucket'],
+            'Key' => $path,
+            'Body' => $handle,
+            'ContentType' => $mime
+        ));
+        $this->_driver->waitUntil('ObjectExists', array(
+            'Bucket' => $this->_config['bucket'],
+            'Key'    => $path
+        ));
+
+        return true;
+    }
+
+    /**
+     * Read contents of file.
+     *
+     * @access    protected
+     * @param    string
+     * @param    resource
+     * @return    bool
+     */
+    protected function _get($path, $handle)
+    {
+        $this->_load();
+
+        $this->_driver->getObject(array(
+            'Bucket' => $this->_config['bucket'],
+            'Key' => $path,
+            'SaveAs' => $handle
+        ));
+
+        return true;
+    }
+
+    /**
+     * Delete
+     *
+     * @access    protected
+     * @param    string
+     * @return    void
+     */
+    protected function _delete($path)
+    {
+        $this->_load();
+
+        $this->_driver->deleteObject(array(
+            'Bucket' => $this->_config['bucket'],
+            'Key' => $path,
+        ));
+    }
+
+    /**
+     * File size
+     *
+     * @access    protected
+     * @param    string
+     * @return    int
+     */
+    protected function _size($path)
+    {
+        $this->_load();
+
+        $result = $this->_driver->headObject(array(
+            'Bucket' => $this->_config['bucket'],
+            'Key' => $path
+        ));
+
+        return $result['ContentLength'];
+    }
+
+    /**
+     * Whether or not file exists
+     *
+     * @access    protected
+     * @param    string
+     * @return    bool
+     */
+    protected function _exists($path)
+    {
+        $this->_load();
+
+        $result = $this->_driver->headObject(array(
+            'Bucket' => $this->_config['bucket'],
+            'Key' => $path
+        ));
+
+        return $result['DeleteMarker'];
+    }
+
+    /**
+     * Get URL
+     *
+     * @access    protected
+     * @param    string
+     * @param    string
+     * @return    string
+     */
+    protected function _url($path, $protocol)
+    {
+        $this->_load();
+
+        return $this->_driver->getObjectUrl($this->_config['bucket'], $path);
+    }
+
+    /**
+     * Get list based on path
+     *
+     * @access    protected
+     * @param    string
+     * @return    mixed
+     */
+    protected function _listing($path, $listing)
+    {
+        $this->_load();
+
+        $result = $this->_driver->listObjects(array(
+            'Bucket' => $this->_config['bucket'],
+            'Prefix' => $path,
+        ));
+
+        foreach ($result->Contents as $file)
+        {
+            $name = (string) $file['Key'];
+
+            $object = Storage_File::factory($name, $this)
+                ->size((int) $file['Size'])
+                ->modified(strtotime($file['LastModified']));
+
+            $listing->set($object);
+        }
+
+        return $listing;
+    }
 }
